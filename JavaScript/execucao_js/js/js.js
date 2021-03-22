cdc458e75a4975447a34eae2966e3d042a479582
ScotLion <80926099+ScotLion@users.noreply.github.com>
2021-03-21 21:33:41 -0300

    Create js.js

---
+++ JavaScript/execucao_js/js/js.js
@@ -0,0 +1,14 @@
+console.log("Execução do JavaScrip Funções")
+
+var nome = "Alessandro";
+var sobrenome = "Silva Lima";
+
+function nomeCompleto (){
+    console.log("Nome Completo: " + nome + " " + sobrenome);
+}
+
+function voceClicou(){
+    alert("Voce clicou em um botão")
+}
+
+
