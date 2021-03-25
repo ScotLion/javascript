241773d9d32fabd4e1a5fbd8d4b4da271c572c5d
ScotLion <80926099+ScotLion@users.noreply.github.com>
2021-03-24 22:15:27 -0300

    Create jsSincona.js

---
+++ sicrona/jsSincona.js
@@ -0,0 +1,13 @@
+console.log("Olá, tudo bem!");
+
+
+
+function msg(){
+    console.log("Executando em 3s");
+}
+setTimeout(msg, 2000);
+
+//repete a cada dois segundo se usar a função setInterval
+//setInterval(msg, 2000);
+
+console.log("Ultima linha");
