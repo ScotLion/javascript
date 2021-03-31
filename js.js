29820df4e8b84354dacb906346d1a4bc29c6e48f
name <alessandrodasilva93@gmail.com>
2021-03-31 17:47:57 -0300

    js

---
+++ js.js
@@ -0,0 +1,11 @@
+var nome = "Alessandro"
+function eventoOnload(){
+    console.log("Evento OnloadDiparado");
+}
+
+function eventoOnClique(){
+    console.log("Sucesso");
+}
+
+setTimeout(eventoOnload, 5000);
+
